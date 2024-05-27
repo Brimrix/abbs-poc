eb009e9f5c3af24bd7986d0aa8c01f32d9f07c7c
Muhammad Salman <salmanhoney987@gmail.com>
2024-05-27 15:35:48 +0500

    Product name on when sidebar expands

--- src/Components/pages/CreateBill.jsx
+++ src/Components/pages/CreateBill.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import LayoutMain from "@/components/LayoutMain";
 import Table from "@/components/Table";
+import ClientSearch from '@/components/ClientSearch';
+import Card from '@/components/Card';
 
 function CreateBill() {
   return (
-    <LayoutMain children={<Table />} />
+    <LayoutMain firstChildren={<ClientSearch />} secondChildren={<Table />} thirdChildren={<Card />} />
   );
 }
 
