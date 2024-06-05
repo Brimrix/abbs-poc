c95748e9b78d02bd7c157d66ccf6db64f835dabe
Muhammad Salman <168727117+SalmanMughal789@users.noreply.github.com>
2024-06-05 16:22:51 +0500

    fix: minor changes (#20)

--- frontend/routes/index.jsx
+++ frontend/routes/index.jsx
@@ -1,25 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
-import Customers from '@/pages/Customers'
-import Dashboard from "@/pages/Dashboard"
-import Invoices from '@/pages/Invoices'
-import Login from '@/pages/Login'
-
-
+import Customers from "@/pages/Customers";
+import Dashboard from "@/pages/Dashboard";
+import Invoices from "@/pages/Invoices";
+import Login from "@/pages/Login";
+import NotFound from "@/pages/NotFound";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Dashboard />
-    }, {
-        path: "customers",
-        element: <Customers />
-    }, {
-        path: 'invoices',
-        element: <Invoices />
-    }, {
-        path: 'login',
-        element: <Login />
-    }
-])
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "customers",
+    element: <Customers />,
+  },
+  {
+    path: "invoices",
+    element: <Invoices />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
-export default router
+export default router;
