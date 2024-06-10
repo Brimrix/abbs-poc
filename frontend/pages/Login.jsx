e509f1a54fbb9e1343e4f4162fca356c6526e92c
Muhammad Salman <salman@brimrix.com>
2024-06-10 15:44:41 +0500

    minor changes

--- frontend/pages/Login.jsx
+++ frontend/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 import "@/assets/styles/LoginStyle.css";
-import {message} from 'antd';
+import { message, Form, Input, Button } from "antd";
 
 const loginURL = import.meta.env.VITE_LOGIN_URL;
 
@@ -19,19 +19,16 @@ const Login = () => {
 
     if (success) {
       message.success("Successfully logged in");
-      navigate("/")
+      navigate("/");
     }
 
     console.log(token);
-
-
   }, [success]);
 
   const getCookieValue = (name) =>
     document.cookie.match("(^|;)\\s*" + name + "\\s*=\\s*([^;]+)")?.pop() || "";
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+  const handleFormSubmit = async () => {
     const response = await fetch(loginURL, {
       method: "POST",
       headers: {
@@ -44,17 +41,24 @@ const Login = () => {
       }),
     });
 
-    if(!response.ok)
-      message.error("Please check your email or password");
+    if (!response.ok)
+    message.error("Please check your email or password");
 
     const data = await response.json();
-    if(response.ok){
+
+    if (response.ok) {
       setToken(data.token);
       setSuccess(response.ok);
     }
+
+
   };
   return (
-    <section className="vh-100 section-bg">
+    <Form
+      name="login-form"
+      onFinish={handleFormSubmit}
+      className="vh-100 section-bg"
+    >
       <div className="container py-5 h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-12 col-md-8 col-lg-6 col-xl-5">
@@ -62,51 +66,58 @@ const Login = () => {
               <div className="card-body p-5 text-start">
                 <h3 className="mb-5">Login ABBS</h3>
 
-                <div className="form-outline mb-4">
-                  <label className="form-label" htmlFor="typeEmailX-2">
-                    Email
-                  </label>
-
-                  <input
-                    value={username}
-                    onInput={(event) => setUsername(event.target.value)}
-                    type="text"
-                    name="username"
+                <Form.Item
+                  name="username"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your email",
+                    },
+                    {
+                      type: "email",
+                      message: "Please input a valid email",
+                    },
+                  ]}
+                >
+                  <Input
                     placeholder="Email"
-                    id="typeEmailX-2"
                     className="form-control form-control-lg"
                   />
-                </div>
+                </Form.Item>
 
-                <div className="form-outline">
-                  <label className="form-label" htmlFor="typeEmailX-2">
-                    Password
-                  </label>
-                  <input
-                    value={password}
-                    onInput={(event) => setPassword(event.target.value)}
-                    name="password"
-                    type="password"
-                    id="password"
+                <Form.Item
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your password",
+                    },
+                  ]}
+                >
+                  <Input.Password
+
                     placeholder="Password"
+                    style={{display: "flex"}}
                     className="form-control form-control-lg"
                   />
-                </div>
+                </Form.Item>
               </div>
 
-                <button
-                  onClick={handleFormSubmit}
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                style={{marginLeft: "180px", height: "40px"}}
                   className="btn button-primary login-btn"
-                  type="submit"
                 >
                   Login
-                </button>
-
+                </Button>
+              </Form.Item>
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </Form>
   );
 };
 
