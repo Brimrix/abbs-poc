016902905bb4aed019f0a27aeff176e89a127996
Waqar Ali <waqar_se@outlook.com>
2024-06-29 23:06:36 +0200

    Improved signup test

--- core/tests/test_user.py
+++ core/tests/test_user.py
@@ -1,5 +1,6 @@
 from core.tests.base import BaseTestCase
 from django.urls import reverse_lazy
+from core import models
 
 
 class UserTestCase(BaseTestCase):
@@ -11,14 +12,15 @@ class UserTestCase(BaseTestCase):
                 "username": "waqar@test.com",
                 "first_name": "Waqar",
                 "last_name": "Ali",
-                "company": self.company.id,
+                "company": "New Company",
                 "phone": "+9232342424",
                 "address": "Mars",
                 "password": "change_me",
                 "confirm_password": "change_me",
             },
         )
-
+        self.assertTrue(models.Company.objects.filter(name="New Company").exists())
+        self.assertTrue(models.User.objects.filter(username="waqar@test.com").exists())
         self.assertEqual(response.status_code, 201)
 
     def test_user_sign_up_weak_password(self):
@@ -29,13 +31,15 @@ class UserTestCase(BaseTestCase):
                 "username": "waqar@test.com",
                 "first_name": "Waqar",
                 "last_name": "Ali",
-                "company": self.company.id,
+                "company": "New Company",
                 "phone": "+9232342424",
                 "address": "Mars",
                 "password": "1234",
                 "confirm_password": "1234",
             },
         )
+        self.assertFalse(models.Company.objects.filter(name="New Company").exists())
+        self.assertFalse(models.User.objects.filter(username="waqar@test.com").exists())
         self.assertEqual(response.status_code, 400)
 
     def test_user_sign_up_no_last_name(self):
@@ -45,11 +49,13 @@ class UserTestCase(BaseTestCase):
             {
                 "username": "waqar@test.com",
                 "first_name": "Waqar",
-                "company": self.company.id,
+                "company": "New Company",
                 "phone": "+9232342424",
                 "address": "Mars",
                 "password": "change_me",
                 "confirm_password": "change_me",
             },
         )
+        self.assertTrue(models.Company.objects.filter(name="New Company").exists())
+        self.assertTrue(models.User.objects.filter(username="waqar@test.com").exists())
         self.assertEqual(response.status_code, 201)
